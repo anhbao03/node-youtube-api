25e2aad763e6884de0faa851e328a75803202e3a
BAODANG\quocb <quocbaoeck97@gmail.com>
2025-03-25 22:50:39 +0700

    no message

---
+++ server.js
@@ -0,0 +1,8 @@
+import http from 'http';
+import app from './app.js';
+
+const server = http.createServer(app);
+
+server.listen(process.env.PORT, () => {
+  console.log(`Server is running on http://localhost:${process.env.PORT}`);
+});
